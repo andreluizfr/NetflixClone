bfdee380021168ca756c48d11025884587978652
Andre Rosa <andre.luizfr2@gmail.com>
2023-06-06 14:26:43 -0300

    first commit

---
+++ server/src/main/java/com/example/NetflixClone/NetflixCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.NetflixClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneApplication.class, args);
+	}
+
+}
