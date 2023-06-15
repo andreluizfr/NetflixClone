ffd7ce2ccd452c1b2bf8fefc412a38ee468bed24
Andre Rosa <andre.luizfr2@gmail.com>
2023-06-15 15:56:34 -0300

    página de contents sendo feita

---
+++ web-app/src/components/ProfileDropdownMenu/index.tsx
@@ -0,0 +1,88 @@
+import './styles.css';
+import profilePicture from '../../assets/img/profile-picture.jpg';
+import * as Popover from '@radix-ui/react-popover';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { motion, useAnimation } from 'framer-motion';
+
+import { Link } from 'react-router-dom';
+
+
+
+export default function ProfileDropdownMenu(): JSX.Element {
+
+    const profileIconControls = useAnimation();
+
+    function animateProfileIcon (){
+        profileIconControls.start({rotate: [45, -45, 45, 0]});
+    };
+    
+    return(
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger className="ProfileDropdownMenuTrigger">
+                <motion.img
+                    className='Profile-icon'
+                    alt='profile icon' 
+                    src={profilePicture}
+                    animate={profileIconControls}
+                    transition={{type:'spring', bounce: 0.8}}
+                />
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+                <DropdownMenu.Content className="ProfileDropdownMenuContent" sideOffset={5} asChild forceMount>
+                    <motion.ul
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{
+                            scale: 1,
+                            opacity: 1,
+                            transition: { type: "spring", duration: 0.3 },
+                        }}    
+                    >
+                        
+                        {
+                            (new Array(10)).fill(0).map((value, index)=>{
+                                return (
+                                    <motion.li className='ProfileDropdownMenuItem'>
+                                        <Link to="#" className='Item-container' key={"profile-"+index}>
+                                            <img className='' src={""} alt="profile icon"/>
+                                            <span className='ProfileName'>{""}</span>
+                                        </Link>
+                                    </motion.li>
+                                )
+                            })
+                        }
+
+                        <motion.li className='ProfileDropdownMenuItem'>
+                            <Link to="#" className='Item-container'>
+                                <img className='' src={""} alt="manage icon"/>
+                                <span className='Option'>Gerenciar perfis </span>
+                            </Link>
+                        </motion.li>
+
+                        <motion.li className='ProfileDropdownMenuItem'>
+                            <Link to="#" className='Item-container'>
+                                <img className='' src={""} alt="transfer icon"/>
+                                <span className='Option'>Transferir perfil</span>
+                            </Link>
+                        </motion.li>
+
+                        <motion.li className='ProfileDropdownMenuItem'>
+                            <Link to="#" className='Item-container'>
+                                <img className='' src={""} alt="account icon"/>
+                                <span className='Option'>Conta</span>
+                            </Link>
+                        </motion.li>
+
+                        <motion.li className='ProfileDropdownMenuItem'>
+                            <Link to="#" className='Item-container'>
+                                <img className='' src={""} alt="help icon"/>
+                                <span className='Option'>Central de Ajuda</span>
+                            </Link>
+                        </motion.li>
+                        
+                    </motion.ul>
+                </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+    );
+}
--- web-app/src/pages/Contents/index.tsx
+++ web-app/src/pages/Contents/index.tsx
@@ -1,10 +1,205 @@
 import './styles.css';
+import logo from '../../assets/svg/logo.svg';
+import playButton from '../../assets/svg/play-button.svg';
+import infoCircle from '../../assets/svg/info-circle.svg';
+import searchIcon from '../../assets/img/search-icon.png';
+import bellIcon from '../../assets/img/bell-icon.png';
+import TheWitcher3Preview from '../../assets/video/the-witcher-3-preview.mp4';
+
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+import ProfileDropdownMenu from '../../components/ProfileDropdownMenu';
+
+import { useEffect } from 'react';
 
 export default function ContentsPage(): JSX.Element {
 
+    useEffect(()=>{
+        const fontLink = document.createElement("link");
+        fontLink.innerHTML = '<link rel="preconnect" href="https://fonts.googleapis.com">'
+        + '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'
+        + '<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">';
+        document.head.appendChild(fontLink);
+    }, []);
+    
     return (
-        <div className='ContentsPage'>
-            
-        </div>  
+        <motion.div className='ContentsPage'>
+
+            <header className='Header'>
+                <div className='Logo-container'>
+                    <Link to="/contents">
+                        <img className='Logo' src={logo} alt="Netflix Logo"/>
+                    </Link> 
+                </div>
+
+                <nav className='Content-navigation'>
+                    <ul>
+                        <li>Início</li>
+                        <li>Séries</li>
+                        <li>Filmes</li>
+                        <li>Bombando</li>
+                        <li>Minha lista</li>
+                        <li>Navegar por idiomas</li>
+                    </ul>
+                </nav>
+
+                <span className='Extra-space'/>
+
+                <nav className='Other-options-container'>
+                    <img src={searchIcon} className='Search-icon' alt="search icon" />
+                    <img src={bellIcon} className='Notifications-icon' alt='Notifications-icon'/>
+                    <ProfileDropdownMenu/>
+                </nav>
+            </header>
+
+            <aside className='Work-preview'>
+                <video className='Background-video' muted autoPlay loop>
+                    <source src={TheWitcher3Preview} type="video/mp4"/>
+                </video>
+                <div className='Work-preview-menu'>
+                    <h1 className='Title'>
+                        The Witcher: temporada 3
+                    </h1>
+                    <div className='Toolbar'>
+                        <button className='Button'>
+                            <img src={playButton} alt="play icon"/>
+                            Assistir
+                        </button>
+
+                        <button className='Button'>
+                            <img src={infoCircle} alt="info circle icon"/>
+                            Mais informações
+                        </button>
+                    </div>
+                </div>
+            </aside>
+
+            <main className='Contents-container'>
+
+                <section className='Section'>
+                    <h1 className='Title'>
+                        Continuar assistindo como {"perfil1"}
+                    </h1>
+                    <div className='Playlist'>
+                        {   
+                            (new Array(20)).fill(0).map((value, index)=>
+                                <img 
+                                    src="https://occ-0-1722-2773.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdzOeOqPEk1M_DWfMF64utFLlsrEMLTsiViL_B4cVd1PuKFzMNbkkathcJBDLghK2pIeAanGHGL_X5pgpNZDnCQPlbK9mVM8UFdjutj-fHXxQASUlsEWhvy25GXqlXGZVz_3.jpg?r=fb8" 
+                                    className='Thumbnail'
+                                    key={"work-"+index}
+                                />
+                            )
+                        }
+                    </div>
+                </section>
+
+                <section className='Section'>
+                    <h1 className='Title'>
+                        Continuar assistindo como {"perfil1"}
+                    </h1>
+                    <div className='Playlist'>
+                        {   
+                            (new Array(20)).fill(0).map((value, index)=>
+                                <img 
+                                    src="https://occ-0-1722-2773.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdzOeOqPEk1M_DWfMF64utFLlsrEMLTsiViL_B4cVd1PuKFzMNbkkathcJBDLghK2pIeAanGHGL_X5pgpNZDnCQPlbK9mVM8UFdjutj-fHXxQASUlsEWhvy25GXqlXGZVz_3.jpg?r=fb8" 
+                                    className='Thumbnail'
+                                    key={"work-"+index}
+                                />
+                            )
+                        }
+                    </div>
+                </section>
+
+                <section className='Section'>
+                    <h1 className='Title'>
+                        Continuar assistindo como {"perfil1"}
+                    </h1>
+                    <div className='Playlist'>
+                        {   
+                            (new Array(20)).fill(0).map((value, index)=>
+                                <img 
+                                    src="https://occ-0-1722-2773.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdzOeOqPEk1M_DWfMF64utFLlsrEMLTsiViL_B4cVd1PuKFzMNbkkathcJBDLghK2pIeAanGHGL_X5pgpNZDnCQPlbK9mVM8UFdjutj-fHXxQASUlsEWhvy25GXqlXGZVz_3.jpg?r=fb8" 
+                                    className='Thumbnail'
+                                    key={"work-"+index}
+                                />
+                            )
+                        }
+                    </div>
+                </section>
+
+                <section className='Section'>
+                    <h1 className='Title'>
+                        Continuar assistindo como {"perfil1"}
+                    </h1>
+                    <div className='Playlist'>
+                        {   
+                            (new Array(20)).fill(0).map((value, index)=>
+                                <img 
+                                    src="https://occ-0-1722-2773.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdzOeOqPEk1M_DWfMF64utFLlsrEMLTsiViL_B4cVd1PuKFzMNbkkathcJBDLghK2pIeAanGHGL_X5pgpNZDnCQPlbK9mVM8UFdjutj-fHXxQASUlsEWhvy25GXqlXGZVz_3.jpg?r=fb8" 
+                                    className='Thumbnail'
+                                    key={"work-"+index}
+                                />
+                            )
+                        }
+                    </div>
+                </section>
+
+                <section className='Section'>
+                    <h1 className='Title'>
+                        Continuar assistindo como {"perfil1"}
+                    </h1>
+                    <div className='Playlist'>
+                        {   
+                            (new Array(20)).fill(0).map((value, index)=>
+                                <img 
+                                    src="https://occ-0-1722-2773.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdzOeOqPEk1M_DWfMF64utFLlsrEMLTsiViL_B4cVd1PuKFzMNbkkathcJBDLghK2pIeAanGHGL_X5pgpNZDnCQPlbK9mVM8UFdjutj-fHXxQASUlsEWhvy25GXqlXGZVz_3.jpg?r=fb8" 
+                                    className='Thumbnail'
+                                    key={"work-"+index}
+                                />
+                            )
+                        }
+                    </div>
+                </section>
+
+            </main>
+
+            <footer>
+                <div className='Row-1'>
+                    Dúvidas? Ligue <a href="#">0800 591 8942</a>
+                </div>
+
+                <div className='Row-2'>
+                    <a href="#">Perguntas frequentes</a>
+                    <a href="#">Central de Ajuda</a>
+                    <a href="#">Conta</a>
+                    <a href="#">Media Center</a>
+                    <a href="#">Relações com investidores</a>
+                    <a href="#">Resgatar cartão pré-pago</a>
+                    <a href="#">Comprar cartão pré-pago</a>
+                    <a href="#">Formas de assistir</a>
+                    <a href="#">Termos de Uso</a>
+                    <a href="#">Privacidade</a>
+                    <a href="#">Preferências de cookies</a>
+                    <a href="#">Informações corporativas</a>
+                    <a href="#">Entre em contato</a>
+                    <a href="#">Teste de velocidade</a>
+                    <a href="#">Avisos legais</a>
+                    <a href="#">Só na Netflix</a>
+                </div>
+                
+                <div className='Row-3'>
+                    <select className="Select-language" defaultValue={"pt"}>
+                        <option value="pt">Português</option>
+                        <option value="en">English</option>
+                    </select>
+                </div>
+
+                <div className='Row-4'>
+                    Netflix Brasil
+                </div>
+            </footer>
+
+        </motion.div>  
     )
 }
