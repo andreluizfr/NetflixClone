3416bea1512d8b8067c18219d788ceebf7676209
André <andre.luizfr2@gmail.com>
2023-07-01 16:53:49 -0300

    colocando dependencias relacionadas a jsf com spring

--- server/src/main/java/com/example/NetflixClone/NetflixCloneApplication.java
+++ server/src/main/java/com/example/NetflixClone/NetflixCloneApplication.java
@@ -2,12 +2,55 @@ package com.example.NetflixClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+//import javax.faces.webapp.FacesServlet;
+import java.util.EventListener;
+//import javax.servlet.Servlet;
+
+import com.sun.faces.config.ConfigureListener;
+
+import jakarta.servlet.Servlet;
+
+
 
 @SpringBootApplication
 public class NetflixCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixCloneApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<Servlet> facesServletRegistration() {
+		// ServletRegistrationBean<Servlet> registration = new
+		// ServletRegistrationBean<Servlet>(new FacesServlet(), "*.xhtml");
+		ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<Servlet>();
+		registration.setLoadOnStartup(1);
+		registration.addUrlMappings("*.jr");
+		return registration;
+	}
+
+	@Bean
+	public ServletContextInitializer servletContextInitializer() {
+		return servletContext -> {
+			servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+			servletContext.setInitParameter("primefaces.THEME", "sunny");
+		};
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<EventListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<EventListener>(new ConfigureListener());
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
