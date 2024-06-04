195841223d2506da6e71d17125f4afd6e2c3caab
André Luiz <andre.luizfr2@gmail.com>
2024-06-04 00:11:25 -0300

    fix: ajuste para leitura correta de variáveis de ambiente

--- media-api/src/main/java/com/example/MediaAPI/MediaAPIApplication.java
+++ media-api/src/main/java/com/example/MediaAPI/MediaAPIApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableFeignClients
 public class MediaAPIApplication{
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(MediaAPIApplication.class, args);
 	}
 }
