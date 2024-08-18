54fd3c3a1f0de202eb8e34e9c985f08edd6c5121
André Luiz <andre.luizfr2@gmail.com>
2024-08-18 01:29:09 -0300

    refactor: movendo servidor de service registry pra o proprio api gateway

--- api-gateway/src/main/java/com/example/ApiGateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/ApiGateway/ApiGatewayApplication.java
@@ -3,11 +3,13 @@ package com.example.ApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableEurekaServer
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
